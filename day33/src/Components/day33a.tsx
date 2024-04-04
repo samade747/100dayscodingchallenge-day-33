849e69114c8304223d5774bafe17effe063cbeba
samade747 <samad.e747@gmail.com>
2024-04-04 18:02:14 +0500

    602

--- day33/src/Components/day33a.tsx
+++ day33/src/Components/day33a.tsx
@@ -1,3 +1,11 @@
 function getCurrentDateFormatted(): string{
-    
+    const now: Date = new Date();
+    const day: string = String(now.getDate()).padStart(2, '0'); // day in 2 digit
+    const month: string = String(now.getMonth() + 1).padStart(2, '0');  // Add 1 because months are 0-index
+    const year: number = now.getFullYear();
+    return `${day}/${month}/${year}`;
 }
+
+
+console.log(getCurrentDateFormatted())
+
---
+++ day33/src/Components/day33b.tsx
@@ -0,0 +1,11 @@
+function datsUnitNewYear(){
+    const today: Date = new Date();
+    const newYear: Date = new Date(today.getFullYear() + 1, 0, 1);
+    const diff: number = newYear.getTime() - today.getTime();
+    const days: number = Math.floor(diff / (1000 * 60 * 60 * 24));
+    return days;
+}
+
+
+
+console.log(datsUnitNewYear() + " days until next year")
